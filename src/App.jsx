b29ad52933798b6ac173b767a27f37954ccfddf0
solpi2 <parkchini@gmail.com>
2025-08-12 15:30:43 +0900

    experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import AboutSection from "./components/AboutSection";
+import ExperienceSection from "./components/ExperienceSection";
 import Header from "./components/Header";
 import ProjectsSection from "./components/ProjectsSection";
 import SkillsSection from "./components/SkillsSection";
+import WritingSection from "./components/WritingSection";
 import portfolioData from "./portfolio-data.json";
 
 const App = () => {
@@ -10,7 +12,17 @@ const App = () => {
       <Header profile={portfolioData.profile} />
       <AboutSection profile={portfolioData.profile} />
       <SkillsSection skills={portfolioData.skills} />
-      <ProjectsSection projects={portfolioData.pro} />
+      <ProjectsSection projects={portfolioData.projects} />
+      <ExperienceSection
+        experience={portfolioData.experience}
+        education={portfolioData.education}
+        certifications={portfolioData.certifications}
+      />
+      <WritingSection
+        writing={portfolioData.writing}
+        talks={portfolioData.talks}
+        openSource={portfolioData.open_source}
+      />
       <footer className="py-8 px-4 text-center">
         <p className="text-sm">
           © 2025 {portfolioData.profile.name}. All right reserved.
