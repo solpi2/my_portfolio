1123378a539055f2815d8205e49991f989d2ba0b
solpi2 <parkchini@gmail.com>
2025-08-12 11:20:45 +0900

    About

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import AboutSection from "./components/AboutSection";
 import Header from "./components/Header";
 import portfolioData from "./portfolio-data.json";
 
 const App = () => {
   return (
-    <div className="bg-red-100 min-h-screen">
+    <div className="min-h-screen">
       <Header profile={portfolioData.profile} />
+      <AboutSection profile={portfolioData.profile} />
       <footer className="py-8 px-4 text-center">
         <p className="text-sm">
           © 2025 {portfolioData.profile.name}. All right reserved.
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,12 @@ import { useEffect } from "react";
 import { FaGithub, FaBlogger, FaLinkedin, FaAt } from "react-icons/fa6";
 
 const Header = ({ profile }) => {
-  useEffect(() => console.log(profile), []);
+    const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <header className="bg-green-100 min-h-screen flex flex-col justify-center items-center text-center px-4">
@@ -80,10 +85,11 @@ const Header = ({ profile }) => {
             "writing",
             "contact",
           ].map((section, index) => (
-            <button
+                        <button
               key={section}
               className="relative group animate-fade-up text-gray-600 transition-all duration-300 font-medium capitalize"
               style={{ animationDelay: `${0.6 + index * 0.1}s` }}
+              onClick={() => scrollToSection(section)}
             >
               {section}
               <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-600 group-hover:w-full transition-all duration-300" />
